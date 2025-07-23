6ba68458323094f62db047f477f065dbd386a706
bindu <davuluribindu995@gmail.com>
2025-07-23 10:22:38 +0530

    modified

---
+++ tfvars/variables.tf
@@ -0,0 +1,42 @@
+variable "ami_id"{
+    type = string
+    default = "ami-09c813fb71547fc4f"
+}
+variable "instance_type"{
+    default = "t2.micro"
+}
+variable "instances" {
+  default = ["mongodb","redis"]
+  
+}
+variable "ec2_tags"{
+    default= {
+        Name = "hello world"
+    }
+}
+variable "environment" {
+  
+}
+variable "common_tags" {
+  default = {
+    project= "roboshop"
+    terraform = true
+  }
+  
+}
+variable "sg_name" {
+  default = "allow-all"
+}
+variable "from_port"{
+    default = 0
+}
+variable "to_port"{
+    default = 0
+}
+variable "cidr_blocks" {
+  default = ["0.0.0.0/0"]
+}
+variable "sg_tags" {
+  default =  "allow_all"
+  
+}
